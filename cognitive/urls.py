5689ecda062c884f143605c9c894bd6fe2292b5d
Karol Działowski <karlososhd@gmail.com>
2019-01-14 20:39:09 +0100

    Filter salary list

--- cognitive/urls.py
+++ cognitive/urls.py
@@ -6,4 +6,5 @@ app_name = 'cognitive'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('salary/', views.SalaryView.as_view(), name='salary'),
+    path('salary/filter/', views.SalaryFilterView, name='salary_filter'),
 ]
--- cognitive/views.py
+++ cognitive/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.template import loader
+from django.http import HttpResponse
 
 from .models import Salary
 
@@ -17,3 +19,25 @@ class SalaryView(generic.ListView):
         Return the list of salaries
         """
         return Salary.objects.all()
+
+
+def SalaryFilterView(request):
+    salary_list = Salary.objects.order_by('worked_years')
+
+    worked_years_start = request.GET['worked_years_start']
+    worked_years_stop = request.GET['worked_years_stop']
+    salary_brutto_start = request.GET['salary_brutto_start']
+    salary_brutto_stop = request.GET['salary_brutto_stop']
+
+    if worked_years_start:
+        salary_list = salary_list.filter(worked_years__gt=worked_years_start)
+    if worked_years_stop:
+        salary_list = salary_list.filter(worked_years__lt=worked_years_stop)
+    if salary_brutto_start:
+        salary_list = salary_list.filter(salary__gt=salary_brutto_start)
+    if salary_brutto_stop:
+        salary_list = salary_list.filter(salary__lt=salary_brutto_stop)
+
+    context = {'salary_list': salary_list}
+    return render(request, 'cognitive/salary.html', context)
+
